975ffda88102c84b3e07671b01e5185f94a49a6e
Chaurasia <Chaurasia@192.168.1.2>
2015-12-30 23:10:14 +0530

    Code tested

---
+++ src/continuitytest/ValetParking.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package continuitytest;
+
+/**
+ *
+ * @author Chaurasia
+ */
+public class ValetParking {
+    
+    String plate;
+    String logintime;
+
+    public String getPlate() {
+        return plate;
+    }
+
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
+
+    public String getLogintime() {
+        return logintime;
+    }
+
+    public void setLogintime(String logintime) {
+        this.logintime = logintime;
+    }
+
+    public String getLogouttime() {
+        return logouttime;
+    }
+
+    public void setLogouttime(String logouttime) {
+        this.logouttime = logouttime;
+    }
+
+    public int getParking_id() {
+        return parking_id;
+    }
+
+    public void setParking_id(int parking_id) {
+        this.parking_id = parking_id;
+    }
+    String logouttime;
+    int parking_id;
+    
+    
+}
